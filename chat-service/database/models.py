32a1ae86cd0de54c411c84bef13bd7f882a338c4
MulhamShaheen <mulhamshaheen.ms@gmail.com>
2025-03-09 23:48:07 +0300

    add alembic and models

---
+++ chat-service/database/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, DateTime, String
+
+Base = declarative_base()
+
+
+class SystemPrompt(Base):
+    __tablename__ = 'system_prompts'
+    prompt_id = Column(Integer, primary_key=True, index=True)
+    prompt_text = Column(String, nullable=True)
+    created_at = Column(DateTime, nullable=False)
+    updated_at = Column(DateTime, nullable=False)
