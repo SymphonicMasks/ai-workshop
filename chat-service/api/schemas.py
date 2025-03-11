b4567606435b6f33eaa5c1995609a4b6c06910b0
Artyom Khalturin <khalturin.artyom@mail.ru>
2025-03-11 17:07:45 +0300

    correct schema

--- chat-service/api/schemas.py
+++ chat-service/api/schemas.py
@@ -2,12 +2,23 @@ from pydantic import (
     BaseModel,
     Field,
 )
-from typing import List, Tuple
+from typing import List, Literal
 
 
 class VersionModel(BaseModel):
     """Версия API"""
     version: str = Field(default=None, title='Версия', description='Номер версии в виде X.Y[.Z]')
 
+class NoteResult(BaseModel):
+    original_note: str
+    played_note: str
+    status: Literal["correct", "wrong", "skipped", "duration+", "duration-"]
+    original_duration: str
+    played_duration: str
+    tact_number: int
+    start_time: float
+    end_time: float
+
+
 class FeedbackRequest(BaseModel):
-    result: List[Tuple[str | None, str | None, str]]
+    result: List[NoteResult]
